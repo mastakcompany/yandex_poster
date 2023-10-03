58a871a176abc080f57d868694909cc905dbcbb6
Jora <joradesktop@joradesktop.com>
2023-10-03 17:12:00 +0700

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'where_to_go.urls'
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 
 TEMPLATES = [
     {
@@ -115,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
