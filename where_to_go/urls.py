9abf898555eb2fec6e93a23adf650646d9922c4b
Jora <joradesktop@joradesktop.com>
2023-10-10 14:57:11 +0700

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 import places.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', places.views.index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
